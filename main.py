f21dcc3269f6f481c06a42902532182157377ce2
abrar-nazib <abrarnazib@gmail.com>
2023-03-18 19:56:14 +0600

    kire bhai?

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import sys
 
 from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QCheckBox)
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication, QSlider, QDial, QWidget, QVBoxLayout, QHBoxLayout
+)
+from PyQt5.QtGui import QPixmap, QPalette, QColor
+
+
+class Color(QWidget):
+    def __init__(self, color: str):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Yo")
-
-        self.setMinimumSize(QSize(200, 200))
-
-        cbox = QCheckBox()
-
-        cbox.setCheckState(Qt.Checked)
-
-        cbox.stateChanged.connect(self.show_state)
-        self.setCentralWidget(cbox)
-
-    def show_state(self, s):
-        print(s)
-        print(Qt.Checked)
+        super(MainWindow, self).__init__()
 
 
 app = QApplication(sys.argv)
