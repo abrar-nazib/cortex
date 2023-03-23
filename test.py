09f2b2be27eab2b37a2c0b38edc4457d52c0d31b
abrar-nazib <abrarnazib@gmail.com>
2023-03-23 12:12:58 +0600

    Import not working

---
+++ test.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import cv2
+
+import time
+
+
+class Worker(QRunnable):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.args = args
+        self.kwargs = kwargs
+
+    @pyqtSlot()
+    def run(self):
+        print("Thread Start")
+        time.sleep(5)
+        print(f"Thread Complete {self.args} {self.kwargs}")
+
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QImage(
+            rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(
+            self.disply_width, self.display_height, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.threadpool = QThreadPool()
+        print("Multithreading with max %d" % self.threadpool.maxThreadCount())
+        widget = QWidget()
+        label = QLabel()
+        label.setPixmap()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+app.exec()
